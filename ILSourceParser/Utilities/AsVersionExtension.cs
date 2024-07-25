63198eb4156579397299f0c64c9cd485dcb29a94
winscripter <142818255+winscripter@users.noreply.github.com>
2024-07-25 14:34:21 +0400

    Add utilities

---
+++ ILSourceParser/Utilities/AsVersionExtension.cs
@@ -0,0 +1,17 @@
+﻿using ILSourceParser.Syntax;
+
+namespace ILSourceParser.Utilities;
+
+/// <summary>
+/// Represents a single extension that provides the method <see cref="AsVersion(VerDirectiveSyntax)"/> to <see cref="VerDirectiveSyntax"/>.
+/// </summary>
+public static class AsVersionExtension
+{
+    /// <summary>
+    /// Converts values of the <c>.ver</c> directive into <see cref="Version"/>.
+    /// </summary>
+    /// <param name="directive">The information about the <c>.ver</c> IL directive.</param>
+    /// <returns>A new instance of <see cref="Version"/>.</returns>
+    public static Version AsVersion(this VerDirectiveSyntax directive) =>
+        new(directive.Major - '0', directive.Minor - '0', directive.Build - '0', directive.Revision - '0');
+}
---
+++ ILSourceParser/Utilities/GetNameOfTypeExtension.cs
@@ -0,0 +1,44 @@
+﻿using ILSourceParser.Syntax;
+
+namespace ILSourceParser.Utilities;
+
+/// <summary>
+/// Represents an extension that provides the method <see cref="GetNameOfType(TypeReferenceSyntax)"/> to the
+/// syntax node <see cref="TypeReferenceSyntax"/>.
+/// </summary>
+public static class GetNameOfTypeExtension
+{
+    /// <summary>
+    /// Returns the full name of the given type reference.
+    /// </summary>
+    /// <param name="syntax">The syntax node that contains the type reference.</param>
+    /// <returns>Full name of the type being referenced.</returns>
+    public static string GetNameOfType(this TypeReferenceSyntax syntax)
+    {
+        if (syntax is NonGenericTypeReferenceSyntax nonGeneric)
+        {
+            string name = nonGeneric.ClassName;
+            return name;
+        }
+        else if (syntax is GenericTypeReferenceSyntax generic)
+        {
+            string name;
+            if (generic.GenericArguments != null)
+            {
+                name =
+                    generic.ClassName +
+                    '`' +
+                    generic.GenericArguments.Parameters.Count().ToString();
+            }
+            else
+            {
+                name = generic.ClassName;
+            }
+            return name;
+        }
+        else
+        {
+            return string.Empty;
+        }
+    }
+}
